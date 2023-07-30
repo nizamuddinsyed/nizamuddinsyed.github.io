2508153c788df7da72c8a93d7af9d5633b57e0c0
nizamuddinsyed <iamsyed.05@gmail.com>
2023-07-31 01:22:23 +0200

    first version portfolio code

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,24 +36,137 @@ const tabs = document.querySelectorAll('[data-target]'),
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
 
-
+let mixerPortfolio = mixitup('.work_container', {
+    selectors: {
+        target: '.work_card'
+    },
+    animation: {
+        duration: 300
+    }
+});
 /*===== Link Active Work =====*/
 
+const linkWork = document.querySelectorAll('.work_item')
+
+function activeWork() {
+    linkWork.forEach(l=> l.classList.remove('active-work'))
+    this.classList.add('active-work')
+}
+
+linkWork.forEach(l=> l.addEventListener("click", activeWork))
 
 /*===== Work Popup =====*/
 
+document.addEventListener("click", (e) => {
+    if(e.target.classList.contains("work_button")){
+        togglePortfolioPopup();
+        portfolioItemDetails(e.target.parentElement);
+    }
 
+})
+
+function togglePortfolioPopup() {
+    document.querySelector(".portfolio_popup").classList.toggle("open");
+}
+
+document.querySelector(".portfolio_popup-close").addEventListener("click", togglePortfolioPopup)
+
+function portfolioItemDetails(portfolioItem) {
+    document.querySelector(".pp_thumbnail img").scr = portfolioItem.querySelector(".work_img").scr;
+    document.querySelector(".portfolio_popup-subtitle span").innerHTML = portfolioItem.querySelector(".work_title").innerHTML;   
+    /*document.querySelector(".portfolio_popup-body span").innerHTML = portfolioItem.querySelector(".portfolio_item-details").innerHTML; */
+}
 /*=============== SERVICES MODAL ===============*/
 
+const modalViews = document.querySelectorAll('.services_modal'),
+modelBtns = document.querySelectorAll('.services_button'),
+modelCloses = document.querySelectorAll('.services_modal-close');
+
+let modal = function(modalClick) {
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modelBtns, i) => {
+    modelBtns.addEventListener('click', () => {
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener("click", () => {
+        modalViews.forEach((modalViews) => {
+            modalViews.classList.remove('active-modal')
+        })
+    })
+})
 
 /*=============== SWIPER TESTIMONIAL ===============*/
 
+let swiper = new Swiper(".testimonials_container", {
+    spaceBetween: 24,
+    loop: true,
+    grabCursor: true,
 
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+        576: {
+          slidesPerView: 2,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 48,
+        },
+      }, 
+  });
 /*=============== INPUT ANIMATION ===============*/
+const inputs = document.querySelectorAll(".input");
 
+function focusFunc() {
+    let parent = this.parentNode;
+    parent.classList.add("focus");
+}
 
+function blurFunc() {
+    let parent = this.parentNode;
+    if(this.value == ""){
+        parent.classList.remove("focus");
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener("focus", focusFunc);
+    input.addEventListener("blur", blurFunc);
+
+}
+)
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+//get al;l sections that have an id defined
+const section = document.querySelectorAll("section[id]");
 
+// add an event listner listning for scroll
+window.addEventListener("scroll", navHighlighter);
+
+function navHighlighter()
+{
+    let scrollY = window.pageYOffset;
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50,
+        sectionId = current.getAttribute("id");
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight)
+        {
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add("active-link")
+        }
+        else
+        {
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove("active-link")
+        }
+    })
+}
 
 /*=============== SHOW SCROLL UP ===============*/
 
